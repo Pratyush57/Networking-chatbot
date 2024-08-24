28821ce87ea206e7e896e087f96132964c082cef
Pratyush57 <143288691+Pratyush57@users.noreply.github.com>
2024-08-25 04:52:01 +0530

    Update server.c

--- server.c
+++ server.c
@@ -9,61 +9,103 @@
 #include <sys/types.h>
 #include <arpa/inet.h>
 
-void * doRecieving(void * sockID){
+int clientCount = 0;
 
-	int clientSocket = *((int *) sockID);
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+struct client{
+
+	int index;
+	int sockID;
+	struct sockaddr_in clientAddr;
+	int len;
+
+};
+
+struct client Client[1024];
+pthread_t thread[1024];
+
+void * doNetworking(void * ClientDetail){
+
+	struct client* clientDetail = (struct client*) ClientDetail;
+	int index = clientDetail -> index;
+	int clientSocket = clientDetail -> sockID;
+
+	printf("Client %d connected.\n",index + 1);
 
 	while(1){
 
 		char data[1024];
 		int read = recv(clientSocket,data,1024,0);
 		data[read] = '\0';
-		printf("%s\n",data);
+
+		char output[1024];
+
+		if(strcmp(data,"LIST") == 0){
+
+			int l = 0;
+
+			for(int i = 0 ; i < clientCount ; i ++){
+
+				if(i != index)
+					l += snprintf(output + l,1024,"Client %d is at socket %d.\n",i + 1,Client[i].sockID);
+
+			}
+
+			send(clientSocket,output,1024,0);
+			continue;
+
+		}
+		if(strcmp(data,"SEND") == 0){
+
+			read = recv(clientSocket,data,1024,0);
+			data[read] = '\0';
+
+			int id = atoi(data) - 1;
+
+			read = recv(clientSocket,data,1024,0);
+			data[read] = '\0';
+
+			send(Client[id].sockID,data,1024,0);			
+
+		}
 
 	}
+
+	return NULL;
 
 }
 
 int main(){
 
-	int clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+	int serverSocket = socket(PF_INET, SOCK_STREAM, 0);
 
 	struct sockaddr_in serverAddr;
 
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(8080);
-	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddr.sin_addr.s_addr = htons(INADDR_ANY);
 
-	if(connect(clientSocket, (struct sockaddr*) &serverAddr, sizeof(serverAddr)) == -1) return 0;
 
-	printf("Connection established ............\n");
+	if(bind(serverSocket,(struct sockaddr *) &serverAddr , sizeof(serverAddr)) == -1) return 0;
 
-	pthread_t thread;
-	pthread_create(&thread, NULL, doRecieving, (void *) &clientSocket );
+	if(listen(serverSocket,1024) == -1) return 0;
+
+	printf("Server started listenting on port 8080 ...........\n");
 
 	while(1){
 
-		char input[1024];
-		scanf("%s",input);
+		Client[clientCount].sockID = accept(serverSocket, (struct sockaddr*) &Client[clientCount].clientAddr, &Client[clientCount].len);
+		Client[clientCount].index = clientCount;
 
-		if(strcmp(input,"LIST") == 0){
+		pthread_create(&thread[clientCount], NULL, doNetworking, (void *) &Client[clientCount]);
 
-			send(clientSocket,input,1024,0);
-
-		}
-		if(strcmp(input,"SEND") == 0){
-
-			send(clientSocket,input,1024,0);
-			
-			scanf("%s",input);
-			send(clientSocket,input,1024,0);
-			
-			scanf("%[^\n]s",input);
-			send(clientSocket,input,1024,0);
-
-		}
-
+		clientCount ++;
+ 
 	}
 
+	for(int i = 0 ; i < clientCount ; i ++)
+		pthread_join(thread[i],NULL);
 
 }
